52ecf5b905c5ae8b4221302d1b50d1096275d4ab
Terje Kolderup <terje@kolderup.net>
2019-07-31 18:22:42 +0200

    x

---
+++ FSharpTest/Test1.fsx
@@ -0,0 +1,4 @@
+﻿#r "/Users/terje/.nuget/packages/fsharp.data/3.1.1/lib/net45/FSharp.Data.dll"
+open FSharp.Data
+
+let items = CsvProvider<"C:\\Users\\terje\\Source\\Repos\\FSharpTest\\FSharpTest\\data.csv">.GetSample()
