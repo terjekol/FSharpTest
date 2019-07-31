e30c6ec69977b2c86cf569d5802d226ce4ab85df
Terje Kolderup <terje@kolderup.net>
2019-07-31 20:49:03 +0200

    x

--- FSharpTest/Test1.fsx
+++ FSharpTest/Test1.fsx
@@ -1,4 +1,21 @@
-﻿#r "/Users/terje/.nuget/packages/fsharp.data/3.1.1/lib/net45/FSharp.Data.dll"
-open FSharp.Data
+﻿#r "/Users/terje/Projects/OsxStocks/packages/FSharp.Data.3.1.1/lib/net45/FSharp.Data.dll"
+//#r "/Users/terje/.nuget/packages/fsharp.data/3.1.1/lib/net45/FSharp.Data.dll"
 
-let items = CsvProvider<"C:\\Users\\terje\\Source\\Repos\\FSharpTest\\FSharpTest\\data.csv">.GetSample()
+open FSharp.Data
+open System
+
+//let items = CsvProvider<"C:\\Users\\terje\\Source\\Repos\\FSharpTest\\FSharpTest\\data.csv">.GetSample()
+type DayStatsRaw = CsvProvider<"/Users/terje/Documents/GitHub/FSharpTest/FSharpTest/data.csv">
+let items = DayStatsRaw.GetSample()
+
+type DayStats =
+    { LocalHigh : decimal
+      LocalLow : decimal
+      Index : decimal
+      Date : DateTime }
+
+let StatsFromRaw (current:DayStatsRaw) previous =
+    { LocalLow = min current.Siste previous.Index
+      LocalHigh = max current.Siste previous.Index
+      Index = current.Siste
+      Date = current.OBX }
