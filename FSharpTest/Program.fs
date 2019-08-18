62abea6148e4b5f5ab4a425a07ace2c72ac08f95
Terje Kolderup <terje@kolderup.net>
2019-08-18 13:05:11 +0200

    Update Program.fs

--- FSharpTest/Program.fs
+++ FSharpTest/Program.fs
@@ -1,31 +1,17 @@
-﻿open System
-open FSharp.Data
+﻿open FSharp.Data
+open System
 
 type Csv = CsvProvider<"/Users/terje/Documents/GitHub/FSharpTest/FSharpTest/data.csv">
-let rawItems = Csv.Load("/Users/terje/Documents/GitHub/FSharpTest/FSharpTest/data.csv")
-type Stats = CsvProvider<"/Users/terje/Documents/GitHub/FSharpTest/FSharpTest/data.csv">.Row
-let x  :Stats = {
-    OBX = 5
-    Siste = 7
-}
-type DayStats =
-    { LocalHigh : decimal
-      LocalLow : decimal
-      Index : decimal
-      Date : DateTime }
+type Stats = Csv.Row
 
-let statsFromRaw (current:DayStatsRaw) previous =
-    { LocalLow = min current.Siste previous.Index
-      LocalHigh = max current.Siste previous.Index
-      Index = current.Siste
-      Date = current.OBX }
-
-let stats = 
-    printf "Stats"
+let firstRow :Stats = 
+    let stats = Csv.Load("/Users/terje/Documents/GitHub/FSharpTest/FSharpTest/data.csv")
+    stats.Rows |> Seq.head
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    stats
+    let row = firstRow
+    printf "%A" row
     0 // return an integer exit code
 
